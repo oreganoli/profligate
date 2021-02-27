d86963c64fea27276df8cf20823a14874b0c7b97
oreganoli <3611916+oreganoli@users.noreply.github.com>
2021-02-27 17:44:07 +0100

    extracted the character shifting logic

--- rust/profligate/src/caesar.rs
+++ rust/profligate/src/caesar.rs
@@ -17,52 +17,36 @@ pub fn encrypt(text: &mut str, key: i16) -> Result<(), CaesarError> {
         text.as_bytes_mut()
     };
     for byte in bytes {
-        match byte {
-            b'a'..=b'z' => {
-                let old_pos = (*byte - b'a') as i16 % ALPHABET_LENGTH;
-                //dbg!(old_pos);
-                let pos = (old_pos as i16 + shift) % ALPHABET_LENGTH;
-                //dbg!(pos);
-                if pos >= 0 {
-                    *byte = pos as u8 + b'a';
-                } else {
-                    *byte = b'z' - (-pos) as u8 + 1;
-                }
-            }
-            b'A'..=b'Z' => {
-                let old_pos = (*byte - b'A') as i16 % ALPHABET_LENGTH;
-                //dbg!(old_pos);
-                let pos = (old_pos as i16 + shift) % ALPHABET_LENGTH;
-                //dbg!(pos);
-                if pos >= 0 {
-                    *byte = pos as u8 + b'A';
-                } else {
-                    *byte = b'Z' - (-pos) as u8 + 1;
-                }
-            }
-            _ => continue,
-        }
+        shift_byte(byte, shift)
     }
     Ok(())
 }
 /// This function applies a Caesar cipher to a raw ASCII byte within the English alphabet. It panics on non-letter bytes.
 /// Note that the ASCII range is within the lower half of a `u8`, and thus also within the positive half of an `i8`.
-fn shift_byte(byte: u8, shift: i8) -> u8 {
+fn shift_byte(byte: &mut u8, shift: i16) {
     match byte {
         b'a'..=b'z' => {
-            if shift >= 0 {
-                ((byte as i8 + shift) as u8 % b'a') + b'a'
+            let old_pos = (*byte - b'a') as i16 % ALPHABET_LENGTH;
+            //dbg!(old_pos);
+            let pos = (old_pos as i16 + shift) % ALPHABET_LENGTH;
+            //dbg!(pos);
+            if pos >= 0 {
+                *byte = pos as u8 + b'a';
             } else {
-                (b'z' as i8 + shift + (byte - b'a') as i8) as u8
+                *byte = b'z' - (-pos) as u8 + 1;
             }
         }
         b'A'..=b'Z' => {
-            if shift >= 0 {
-                ((byte as i8 + shift) as u8 % b'A') + b'A'
+            let old_pos = (*byte - b'A') as i16 % ALPHABET_LENGTH;
+            //dbg!(old_pos);
+            let pos = (old_pos as i16 + shift) % ALPHABET_LENGTH;
+            //dbg!(pos);
+            if pos >= 0 {
+                *byte = pos as u8 + b'A';
             } else {
-                (b'Z' as i8 + shift + (byte - b'A') as i8) as u8
+                *byte = b'Z' - (-pos) as u8 + 1;
             }
         }
-        _ => panic!("The byte being shifted isn't an ASCII letter."),
+        _ => (),
     }
 }
