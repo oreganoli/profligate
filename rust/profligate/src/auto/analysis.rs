3e6365abfc998bc51e033eea2e452bdee78a9c31
oreganoli <3611916+oreganoli@users.noreply.github.com>
2021-02-27 20:02:34 +0100

    implemented character frequency check

--- rust/profligate/src/auto/analysis.rs
+++ rust/profligate/src/auto/analysis.rs
@@ -1,7 +1,24 @@
 #[cfg(test)]
 mod tests;
 
-/// Returns the most frequently occurring character in the given string. Case-insensitive; assumes the text is ASCII.
+/// Returns the most frequently occurring character in the given string. Case-insensitive, returns a lowercase char; assumes the text is ASCII.
 pub fn most_frequent_char(text: &str) -> u8 {
-    unimplemented!()
+    let mut table = [0; 26];
+    let bytes = text.as_bytes();
+    for byte in bytes {
+        match *byte {
+            b'a'..=b'z' => table[(*byte - b'a') as usize] += 1,
+            b'A'..=b'Z' => table[(*byte - b'A') as usize] += 1,
+            _ => (),
+        }
+    }
+    let mut max = 0;
+    let mut max_index = 0;
+    for i in 0..table.len() {
+        if table[i] > max {
+            max = table[i];
+            max_index = i as u8;
+        }
+    }
+    max_index + b'a'
 }
